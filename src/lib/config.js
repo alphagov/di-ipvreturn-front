1f5d279533a41f852b3188e995958d053761e535
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2023-04-25 12:22:41 +0100

    Unnecessary files removed

---
+++ src/lib/config.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+
+module.exports = {
+  API: {
+    BASE_URL: process.env.API_BASE_URL || "http://localhost:8090",
+    PATHS: {
+      SESSION: "/session",
+      AUTHORIZATION: "/authorization",
+      SAVE_F2FDATA: "/documentSelection",
+    },
+  },
+  PROXY_API: {
+    BASE_URL: process.env.PROXYURL,
+    PATHS: {
+      POST_OFFICE: "/postoffice/v1/locations/search"
+    },
+  },
+  APP: {
+    BASE_URL: process.env.EXTERNAL_WEBSITE_HOST || "http://localhost:8000",
+    PATHS: {
+      F2F: "/",
+      UK_PASSPORT_DETAILS: "/ukPassportDetails",
+      NON_UK_PASSPORT_DETAILS: "/nonUKPassportDetails",
+      PHOTOCARD_DL_DETAILS: "/ukPhotocardDlDetails",
+      BRP_DETAILS: "/brpDetails",
+      EU_PHOTOCARD_DL_DETAILS: "/euPhotocardDlDetails",
+      EEA_IDENTITY_CARD_DETAILS: "/eeaIdentityCardDetails",
+      NO_PHOTO_ID: "https://signin.account.gov.uk/no-photo-id",
+      EXPIRED_ID: "/photoIdExpiry"
+    },
+    PHOTO_ID_OPTIONS: {
+      UK_PASSPORT: "ukPassport",
+      NON_UK_PASSPORT: "nonUkPassport",
+      UK_PHOTOCARD_DL: "ukPhotocardDl",
+      BRP: "brp",
+      EU_PHOTOCARD_DL: "euPhotocardDl",
+      EEA_IDENTITY_CARD: "eeaIdentityCard",
+      NO_PHOTO_ID: "noPhotoId"
+    },
+    UK_PASSPORT_HINT: "If your UK passport has expired, you can still use it to prove your identity up to 18 months after the expiry date.",
+    EU_PHOTOCARD_DL_HINT: "This must be a plastic photocard. You cannot use a paper or handwritten license.",
+    EEA_IDENTITY_CARD_HINT: "This must be a plastic photocard. You cannot use a paper or handwritten national identity card.",
+    PHOTO_ID_EXPIRY_OPTIONS: {
+      RE_ENTER_DETAILS: "reEnterDetails",
+      CHOOSE_DIFFERENT_PHOTO_ID: "chooseDifferentPhotoId",
+      PROVE_IDENTITY_ANOTHER_WAY: "proveIdentityAnotherWay"
+    },
+    ANALYTICS: {
+      ID: process.env.GTM_ID,
+      DOMAIN: process.env.ANALYTICS_DOMAIN || "localhost",
+    },
+  },
+  PORT: process.env.PORT || 5030,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  SESSION_TABLE_NAME: process.env.SESSION_TABLE_NAME,
+  SESSION_TTL: process.env.SESSION_TTL || 7200000, // two hours in ms
+  REDIS: {
+    SESSION_URL: process.env.REDIS_SESSION_URL,
+    PORT: process.env.REDIS_PORT || 6379,
+  },
+};
---
+++ src/lib/settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  setAPIConfig: ({ app, baseUrl, sessionPath, authorizationPath }) => {
+    app.set("API.BASE_URL", baseUrl);
+    app.set("API.PATHS.SESSION", sessionPath);
+    app.set("API.PATHS.AUTHORIZATION", authorizationPath);
+  },
+
+  setProxyAPIConfig: ({ app, postOfficeProxyUrl }) => {
+    app.set("PROXY_API.PATHS.POST_OFFICE", postOfficeProxyUrl);
+  },
+
+  setOAuthPaths: ({ app, entryPointPath }) => {
+    app.set("APP.PATHS.ENTRYPOINT", entryPointPath);
+  },
+};
